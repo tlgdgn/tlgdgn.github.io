055e695c5b538aabd62c1dc87987bd451ee4d3bb
Tolga Doğan <118059132+tlgdgn@users.noreply.github.com>
2024-05-12 03:46:59 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const heart = document.querySelector('.heart');
-const secretMessage = document.querySelector('.secret-message');
-
-heart.addEventListener('click', () => {
-  secretMessage.style.opacity = 1;
-  // Replace this with your actual secret message
-  secretMessage.textContent = "You're the peanut butter to my jelly!";
-});
